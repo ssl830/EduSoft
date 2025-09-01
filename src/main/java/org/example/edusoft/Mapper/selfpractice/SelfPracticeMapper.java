1e62b5331c025f08ba98c59775045df4b6685eb3
sjy <23371095@buaa.edu.cn>
2025-09-01 10:23:27 +0800

    init micro_course_service

--- src/main/java/org/example/edusoft/Mapper/selfpractice/SelfPracticeMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.edusoft.mapper.selfpractice;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.example.edusoft.entity.selfpractice.SelfPractice;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SelfPracticeMapper extends BaseMapper<SelfPractice> {
-} 
--- src/main/java/org/example/edusoft/entity/user/PasswordChangeRequest.java
+++
@@ -1,5 +0,0 @@
-package org.example.edusoft.entity.user;
-
-public class PasswordChangeRequest {
-    
-}
