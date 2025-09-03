41357ccea572b944dda5585e6ace676d8c2b842c
sjy <23371095@buaa.edu.cn>
2025-09-04 03:00:17 +0800

    init: user microservice

--- content-service/src/main/java/org/example/edusoft/content/service/file/FilePreviewService.java
+++
@@ -1,8 +0,0 @@
-package org.example.edusoft.content.service.file;
-
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface FilePreviewService {
-    void previewFile(Long fileId, HttpServletResponse response);
-}
-
